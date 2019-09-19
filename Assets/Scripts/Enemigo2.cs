ed0b2467a8b491ac65380e118917f07021dfaef5
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 01:17:38 -0500

    update programacion enemigo

---
+++ Assets/Scripts/Enemigo2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo2: MonoBehaviour
+{
+
+    public float velocidad;
+    private Waypoints wpoints;
+
+    private int contador;
+
+    void Start()
+    {
+        wpoints = GameObject.FindGameObjectWithTag("Waypoints2").GetComponent<Waypoints>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, wpoints.waypoints[contador].position, velocidad * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, wpoints.waypoints[contador].position) < 0.1f)
+        {
+            contador++;
+        }
+    }
+}
