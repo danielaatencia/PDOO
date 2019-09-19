8f5ec24c65854d5832a9b064c0638ca36e79949c
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 09:41:30 -0500

    programacion de nueva oleada

---
+++ Assets/Scripts/nueva_oleada.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nueva_Oleada : MonoBehaviour
+{
+    public static ArrayList NuevaOleada = new ArrayList();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject unidad = GameObject.Find("unidad_01");
+        GameObject temporal;
+        Vector3 incremento = new Vector3(0, 1);
+        Vector3 posicionInicial = unidad.transform.position;
+
+        for (int i = 0; i < 10; i++)
+        {
+            temporal = (GameObject)Instantiate(unidad, posicionInicial + incremento, Quaternion.identity);
+            posicionInicial = temporal.transform.position;
+            NuevaOleada.Add(temporal);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
