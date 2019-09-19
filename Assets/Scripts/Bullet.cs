eaff93a0b9e6f03d1efeb4f9ffdb389a6ad11ee1
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 10:12:04 -0500

    prueba programacion optimizacion de balas

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform objetivo;
+    public float velocidad = 20;
+
+    private Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Enemigo");
+        objetivo = go.transform;
+        // rotate the projectile to aim the target:
+        myTransform.LookAt(objetivo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // distance moved since last frame:
+        float seguirObjetivo = velocidad * Time.deltaTime;
+        // translate projectile in its forward direction:
+        myTransform.Translate(Vector3.forward * seguirObjetivo);
+    }
+}
