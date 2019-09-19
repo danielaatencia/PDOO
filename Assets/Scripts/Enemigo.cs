94369f37449f5eede6734983d0b38c8e2e62cc13
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 01:15:32 -0500

    programacion enemigo

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo: MonoBehaviour
+{
+
+    public float velocidad;
+    private Waypoints wpoints;
+
+    private int contador;
+
+    void Start()
+    {
+        wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, wpoints.waypoints[contador].position, velocidad * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, wpoints.waypoints[contador].position) < 0.1f)
+        {
+            contador++;
+        }
+    }
+}
