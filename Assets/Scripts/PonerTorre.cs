70a755fc5469b199fdfb18dc1cfa3e2ac5fd46d3
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 00:37:28 -0500

    Creacion de menu

---
+++ Assets/Scripts/PonerTorre.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PonerTorre : MonoBehaviour
+{
+    public GameObject torre;
+
+    private void OnMouseDown()
+    {
+        GameObject temp;
+        Vector3 pos = this.transform.position;
+        pos.y = pos.y + .4f;
+        temp = Instantiate(torre);
+        temp.transform.position = pos;
+        temp.layer = 5;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+
+    public void CargarNivel()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+
+}
