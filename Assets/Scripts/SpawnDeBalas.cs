03d376d75e495fef6e84ab952ab9009bf46e1320
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 09:43:45 -0500

    programacion disparo de torres

---
+++ Assets/Scripts/SpawnDeBalas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDeBalas : MonoBehaviour
+{
+
+    public GameObject bala;
+    public bool aparecer = false;
+    public float tiempoReaparicion;
+    public float frecuencia;
+    //test de deteccion
+    public GameObject enemigo;
+    private float distancia_umbral = 4;
+
+    void Start()
+    {
+        InvokeRepeating("Aparicion", tiempoReaparicion, frecuencia);
+    }
+
+    public void Aparicion()
+    {
+        //test de deteccion
+        float dist = (enemigo.transform.position - this.transform.position).magnitude;
+        if (dist <= distancia_umbral)
+        //test de deteccion
+        {
+            Instantiate(bala, transform.position, transform.rotation);
+        if (aparecer)
+        {
+            CancelInvoke("Aparicion");
+        }
+        }
+    }
+}
