7ff5a4c859f4cc2628cec514c36b0df8d695e290
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 10:12:30 -0500

    prueba programacion optimizacion de disparos

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+
+    public GameObject bala;
+    public bool aparecer = false;
+    public float tiempoReaparicion;
+    public float frecuencia;
+
+    void Start()
+    {
+        InvokeRepeating("Aparicion", tiempoReaparicion, frecuencia);
+    }
+
+    public void Aparicion()
+    {
+        Instantiate(bala, transform.position, transform.rotation);
+        if (aparecer)
+        {
+            CancelInvoke("Aparicion");
+        }
+    }
+}
