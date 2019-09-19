1c82ace70d32c773bafbe4b1defc854a97589ac4
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 09:40:14 -0500

    programación salud

---
+++ Assets/Scripts/Salud.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salud : MonoBehaviour
+{
+    public int salud = 20;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Bala"))
+        {
+            salud -= 2;
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (salud == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
