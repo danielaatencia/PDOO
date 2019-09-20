388a05f0897a3f57abf2e40e76cdd8a3bf5ae4b2
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 22:31:47 -0500

    programacion de monedas (puntos)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+
+{
+
+    public static int Coin = 1000;
+    public string coinString = "Coins";
+
+    public Text TextCoin;
+
+    public static GameController Gamecontroller;
+
+    private void Awake()
+    {
+        Gamecontroller = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TextCoin != null)
+        {
+            TextCoin.text = coinString + Coin.ToString();
+        } 
+    }
+}
