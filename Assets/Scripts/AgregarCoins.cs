7d39a814fa9f0f291418fe6d50f5332e3d49b00e
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 22:38:44 -0500

    programacion de agregar monedas

---
+++ Assets/Scripts/AgregarCoins.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgregarCoins : MonoBehaviour
+{
+
+    public int Moneda = 1000;
+
+    private void OnDestroy()
+    {
+        GameController.Coin += Moneda;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
