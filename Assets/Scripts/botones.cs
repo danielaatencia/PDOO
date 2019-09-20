4d5197595c79461783248fc769d50193dcef66a0
Daniela Atencia <daniela.atencia@upb.edu.co>
2019-09-19 23:22:36 -0500

    programacion de botones de pantallas ganar y perder

---
+++ Assets/Scripts/botones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class updateMenu : MonoBehaviour
+{
+    public void CargarNivel()
+    {
+        SceneManager.LoadScene("tower 2");
+    }
+
+    public void AlMenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+}
